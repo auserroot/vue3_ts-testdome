4e6ef8e1232f82f6020910f1e8c5f5952ffbabad
fuzy <fuzy@upup.com>
2021-05-21 09:38:05 +0800

    build

---
+++ src/components/test.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<button @click="headleClick">{{ count }}</button>
+	</div>
+</template>
+
+<script lang="ts">
+import { ref , onMounted} from 'vue';
+export default {
+	setup() {
+		const count:any = ref(0); //双向绑定数据、初始化值
+		// console.log(count)//RefImpl 对象
+		const headleClick:any = () => {
+			count.value++//取值自增
+		};
+		
+		onMounted(()=>{
+			setInterval(()=>{
+				headleClick()
+			},1000)
+		})
+		return {
+			count,
+			headleClick
+		};
+	},
+	
+};
+</script>
+
+<style></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+	<Test/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Test from '@/components/test.vue'
+@Options({
+  components: {
+    HelloWorld,
+	Test
+  },
+})
+export default class Home extends Vue {}
+</script>
